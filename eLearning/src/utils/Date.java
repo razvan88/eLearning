b116637e732941a6be5b2485392a30b051903b51
Razvan Nedelcu <razvannedelcu2004@yahoo.com>
2014-06-02 15:46:04 +0300

    filtering and sorting added for deadlines. !TO BE TESTED!

---
+++ eLearning/src/utils/Date.java
@@ -0,0 +1,84 @@
+package utils;
+
+public class Date {
+	
+	/**
+	 * @param date1 first date to be compared (format: YYYY-MM-DD)
+	 * @param date2 second date to be compared (format: YYYY-MM-DD)
+	 * @return true if date1 is before date2, false otherwise
+	 */
+	public static boolean dayDateBefore(String date1, String date2) {
+		String[] dateTokens = date1.split("-");
+		int year1 = Integer.parseInt(dateTokens[0]);
+		int month1 = Integer.parseInt(dateTokens[1]);
+		int day1 = Integer.parseInt(dateTokens[2]);
+		
+		dateTokens = date2.split("-");
+		int year2 = Integer.parseInt(dateTokens[0]);
+		int month2 = Integer.parseInt(dateTokens[1]);
+		int day2 = Integer.parseInt(dateTokens[2]);
+		
+		if(year1 < year2 || 
+				(year1 == year2 && month1 < month2) ||
+				(year1 == year2 && month1 == month2 && day1 < day2)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @param date1 first date to be compared (format: YYYY-MM-DD)
+	 * @param date2 second date to be compared (format: YYYY-MM-DD)
+	 * @return true if date1 equals date2, false otherwise
+	 */
+	public static boolean sameDayDate(String date1, String date2) {
+		String[] dateTokens = date1.split("-");
+		int year1 = Integer.parseInt(dateTokens[0]);
+		int month1 = Integer.parseInt(dateTokens[1]);
+		int day1 = Integer.parseInt(dateTokens[2]);
+		
+		dateTokens = date2.split("-");
+		int year2 = Integer.parseInt(dateTokens[0]);
+		int month2 = Integer.parseInt(dateTokens[1]);
+		int day2 = Integer.parseInt(dateTokens[2]);
+		
+		return year1 == year2 && 
+				month1 == month2 && 
+				day1 == day2;
+	}
+	
+	/**
+	 * @param date1 first date to be compared (format: YYYY-MM-DD HH:MM:SS)
+	 * @param date2 second date to be compared (format: YYYY-MM-DD HH:MM:SS)
+	 * @return true if date1 is before date2, false otherwise
+	 */
+	public static boolean timeDateBefore(String date1, String date2) {
+		String[] day1 = date1.split(" ");
+		String[] day2 = date2.split(" ");
+		
+		if(!sameDayDate(day1[0], day2[0])) {
+			return dayDateBefore(day1[0], day2[0]);
+		}
+		
+		//check the time
+		String[] time1 = day1[1].split(":");
+		String[] time2 = day2[1].split(":");
+
+		int hour1 = Integer.parseInt(time1[0]);
+		int minute1 = Integer.parseInt(time1[1]);
+		int second1 = Integer.parseInt(time1[2]);
+				
+		int hour2 = Integer.parseInt(time2[0]);
+		int minute2 = Integer.parseInt(time2[1]);
+		int second2 = Integer.parseInt(time2[2]);
+		
+		if(hour1 < hour2 ||
+				(hour1 == hour2 && minute1 < minute2) ||
+				(hour1 == hour2 && minute1 == minute2 && second1 < second2)) {
+			return true;
+		}
+		
+		return false;
+	}
+}
--- eLearning/src/webserviceResources/getters/AllDeadlinesResource.java
+++ eLearning/src/webserviceResources/getters/AllDeadlinesResource.java
@@ -1,5 +1,10 @@
 package webserviceResources.getters;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
@@ -9,6 +14,7 @@ import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
 import utils.ConfigurationSettings;
+import utils.Date;
 import database.DBConnection;
 import database.DBConnectionManager;
 import database.DBUtils;
@@ -19,17 +25,55 @@ public class AllDeadlinesResource extends ServerResource {
 	public String getAllDeadlines(Representation entity) {
 		Form request = new Form(this.getRequestEntity());
 		JSONObject info = JSONObject.fromObject(request.getValues("info"));
-		
+
 		int schoolId = info.getInt("schoolId");
 		int userId = info.getInt("userId");
-		
+
 		String database = ConfigurationSettings.getSchoolDatabaseName(schoolId);
-		DBConnection dbConnection = DBConnectionManager.getConnection(schoolId, database);
-		
+		DBConnection dbConnection = DBConnectionManager.getConnection(schoolId,
+				database);
+
 		JSONArray deadlines = DBUtils.getAllDeadlines(dbConnection, userId);
-		
-		// TODO - sort and keep only the valid deadlines
-		
-		return deadlines.toString();
+
+		Calendar cal = new GregorianCalendar();
+		int currentDay = cal.get(Calendar.DAY_OF_MONTH);
+		int currentMonth = cal.get(Calendar.MONTH) + 1;
+		int currentYear = cal.get(Calendar.YEAR);
+		String currentDayDate = String.format("%s-%s-%s", currentYear,
+				currentMonth, currentDay);
+		JSONArray filteredDeadlines = new JSONArray();
+
+		// keep only the valid deadlines
+		for (int i = 0; i < deadlines.size(); i++) {
+			JSONObject deadline = deadlines.getJSONObject(i);
+
+			String dayDate = deadline.getString("deadline").split(" ")[0];
+			if (Date.dayDateBefore(dayDate, currentDayDate)) {
+				continue;
+			}
+
+			filteredDeadlines.add(deadline);
+		}
+
+		// sort the deadlines
+		JSONObject[] array = (JSONObject[]) JSONArray
+				.toArray(filteredDeadlines);
+		Arrays.sort(array, new Comparator<JSONObject>() {
+			@Override
+			public int compare(JSONObject o1, JSONObject o2) {
+				String date1 = o1.getString("deadline");
+				String date2 = o2.getString("deadline");
+
+				return Date.timeDateBefore(date1, date2) ? -1 : 1;
+			}
+		});
+
+		// remove unsorted elements and add them in order
+		filteredDeadlines.clear();
+		for (JSONObject obj : array) {
+			filteredDeadlines.add(obj);
+		}
+
+		return filteredDeadlines.toString();
 	}
 }
